e874114d8abe284e15676a80cea94b74ba74175c
MartinLau <Martinla@livemail.tw>
2017-01-15 17:02:53 +0800

    new

---
+++ MSCodesignProgram/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MSCodesignProgram
+//
+//  Created by MartinLau on 2017/1/15.
+//  Copyright © 2017年 Martin Studio. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ MSCodesignProgram/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  MSCodesignProgram
+//
+//  Created by MartinLau on 2017/1/15.
+//  Copyright © 2017年 Martin Studio. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
